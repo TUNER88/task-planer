5705b25261fb63a7a4e1f07ff9d3b34bf54d5817
Anton Pauli <apauli@team.mobile.de>
2019-06-05 00:29:46 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App'
+import rootReducer from './reducers'
+import initialState from './config/initialState'
+
+const store = createStore(rootReducer, initialState)
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
