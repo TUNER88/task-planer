474e797ea48dc569d1385527cad75e81e65b1b9c
Anton Pauli <apauli@ebay.com>
2019-07-31 21:55:26 +0200

    reduce icon size

--- src/components/TaskList/AddTaskButton.js
+++ src/components/TaskList/AddTaskButton.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from 'antd-mobile'
 
-import { IoMdAdd } from 'react-icons/io'
+import { IoIosAdd } from 'react-icons/io'
 
 import styles from './AddTaskButton.module.css'
 
 const AddTaskButton = ({ onClick }) => {
   return (
     <List.Item
-      thumb={<IoMdAdd size={'21px'} style={{ color: '#666' }} />}
+      thumb={<IoIosAdd size={'21px'} style={{ color: '#666' }} />}
       onClick={onClick}
     >
       <span className={styles.text}>Add new</span>
