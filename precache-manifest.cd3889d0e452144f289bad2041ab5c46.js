d3b62585bd955bead03db799976499cc94f2cda6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-28 22:15:02 +0000

    Deploy tp.pauli.cloud to github.com/TUNER88/task-planer.git:gh-pages

---
+++ precache-manifest.cd3889d0e452144f289bad2041ab5c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdeb8943a58015d2267ea7fd6a733543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77de9da685f7f3614f2",
+    "url": "/static/css/2.01982b57.chunk.css"
+  },
+  {
+    "revision": "2e633ccb7042f15c7000",
+    "url": "/static/css/main.06f03965.chunk.css"
+  },
+  {
+    "revision": "b77de9da685f7f3614f2",
+    "url": "/static/js/2.15e6cc50.chunk.js"
+  },
+  {
+    "revision": "2e633ccb7042f15c7000",
+    "url": "/static/js/main.ec091b0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
